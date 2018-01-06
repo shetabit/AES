41f06b253f7533a385b1ac112b7b3ecf01526471
Hashem Mo <hashemm364@gmail.com>
2018-01-06 14:17:31 +0330

    Create AES.php

---
+++ AES.php
@@ -0,0 +1,74 @@
+<?php
+
+class AES
+{
+    /**
+     * @var string
+     */
+    protected $cipher;
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var null | string
+     */
+    protected $iv;
+
+    /**
+     * AES constructor.
+     *
+     * @param $key
+     * @param string $cipher
+     * @param null|string $iv
+     */
+    public function __construct($key, $cipher = 'AES-256-CBC', $iv = null)
+    {
+        $this->cipher = $cipher;
+        $this->key = $key;
+        $this->iv = $iv;
+    }
+
+    /**
+     * Encrypt the given data.
+     *
+     * @param string $data
+     * @return string
+     *
+     */
+    public function encrypt($data)
+    {
+        $iv = $this->iv ? $this->iv : random_bytes(openssl_cipher_iv_length($this->cipher));
+
+        $encrypted = openssl_encrypt($data, $this->cipher, base64_decode($this->key), 0, base64_decode($iv));
+
+        if ($this->iv == null) {
+            return $encrypted . ':' . base64_encode($iv);
+        }
+        return $encrypted;
+    }
+
+    /**
+     * Decrypt the given data.
+     *
+     * @param string $data
+     * @param bool $randomIv
+     * @return string
+     */
+    public function decrypt($data, $randomIv = false)
+    {
+        if ($randomIv) {
+            // To decrypt, separate the encrypted data from the initialization vector ($iv).
+            $parts = explode(':', $data);
+            $encrypted = $parts[0];
+            $iv = base64_decode($parts[1]);
+        } else {
+            $encrypted = $data;
+            $iv = base64_decode($this->iv);
+        }
+        $decrypted = openssl_decrypt($encrypted, $this->cipher, base64_decode($this->key), 0, $iv);
+        return $decrypted;
+    }
+}
